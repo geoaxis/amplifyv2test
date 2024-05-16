57fdbc943984a9510aa0830a28f4f541a395d08f
Shahzada Hatim <geoaxis@gmail.com>
2024-05-16 08:19:51 +0200

    A basic login application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Auth from "@/src/components/auth/Auth"
+import NavBar from "@/src/components/NavBar"
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <NavBar />
+        <Auth>
+        {children}
+        </Auth>
+        </body>
     </html>
   );
 }
---
+++ src/app/signin/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import AuthClient from '@/src/components/auth/AuthClient';
+
+const SignIn = () => {
+    return <AuthClient />;
+}
+
+export default SignIn;
